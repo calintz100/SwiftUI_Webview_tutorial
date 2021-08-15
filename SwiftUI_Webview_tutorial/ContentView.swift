3f74bcbee6b4ed7a56df76bb67209a25934a0e70
heavylucky <calintz100@gmail.com>
2021-08-16 02:01:27 +0900

    Initial Commit

---
+++ SwiftUI_Webview_tutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Webview_tutorial
+//
+//  Created by shin on 2021/08/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
